934f4a46eff3568795c4bc310ea9c098fed4d4bc
Ruslan Abdiev <90495787+deadlockkk@users.noreply.github.com>
2025-10-02 10:52:10 +0700

    Update Huffman.cpp

--- Labs/Huffman.cpp
+++ Labs/Huffman.cpp
@@ -1,3 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <vector>   // шаблонный класс-массив(класс-вектор)
+#include <map>  // шаблонный класс-словарь
+#include <list> // шаблонный класс-список
+
+using namespace std;
+
+class Haffman_Tree { //вспомогательный класс дерево
+public:
+  int key;
+  char s;
+  
+}
 main()
   { std::cout«"Hello world";
 // тут скоро будет Алгоритм хаффмана :)))) 
